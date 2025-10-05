5f875e859baa818e14daa1849088f3eb3cdf0d31
Your Name <your_email@example.com>
2025-10-05 16:51:56 +0530

    fix: Remove unnecessary whitespace in Navbar component

--- royalbidz.client/src/components/Navbar.jsx
+++ royalbidz.client/src/components/Navbar.jsx
@@ -53,6 +53,7 @@ function Navbar() {
           <span className="notification-badge">3</span>
         </a>
         
+        
         {/* User Profile Icon */}
         <a href="#profile" className="profile-icon" aria-label="User Profile">
           <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
